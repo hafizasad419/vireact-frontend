d5649864a8fd7cfccf74030511a8a5f0edc87900
Asad Riaz <hafizasad419@gmail.com>
2025-12-14 23:38:12 +0500

    use type imports for RefObject

--- src/components/Header/UserHeader/ProfileDropdown/index.tsx
+++ src/components/Header/UserHeader/ProfileDropdown/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, RefObject } from 'react';
+import { useEffect, useRef, type RefObject } from 'react';
 import { FaSignOutAlt } from 'react-icons/fa';
 
 interface ProfileDropdownProps {
